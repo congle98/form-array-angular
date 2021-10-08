d87494a69ad3c749b42c045e33cfc64d266de1a7
Le Thanh Cong (PM) <conglt@nms.vn>
2021-10-08 16:48:43 +0700

    oke

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'form-array';
+export class AppComponent implements OnInit{
+  formArray = new FormGroup({
+    'hobbies': new FormArray([])
+  });
+  constructor() { }
+  ngOnInit() {
+  }
+  addFormArray() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.formArray.get('hobbies')).push(control);
+  }
 }
